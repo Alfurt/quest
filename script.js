af07031c620497f040893428ba8295c05f6ff06f
Alfurt <43576930+Alfurt@users.noreply.github.com>
2019-12-14 23:37:51 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var questNum = 0;
 var rightAns = 0;
-var phrase = ["anus", "jopa", "asd", "1010101010"];
+var phrase = ["anus", "jopa", "asd", "1010101010", "5 б б", "сковречник", "домик для птиц", "в лесу", "карта", "фонарик", "послание", "секрет", "тайна", "свечка", "свеча", "огонь", "карта", "карта и свеча", "карта и свечка", "ничего", "жопа", "туса",];
 
 answer.onclick = function() {
 document.getElementById("title").innerHTML = phrase[Math.floor(Math.floor(Math.random() * Math.floor(phrase.length)))];
@@ -32,39 +32,39 @@ but.onclick = function()
     // -- Здесь ответы --
     if(questNum == 1)
     {
-      compare = ["1"];
+      compare = ["свечка", "свеча", "огонь", "карта", "карта и свеча", "карта и свечка"];
     }
     else if(questNum == 2)
     {
-      compare = ["1"];
+      compare = ["5 б б"];
     }
     else if(questNum == 3)
     {
-      compare = ["1"];
+      compare = ["ничего"];
     }
     else if(questNum == 4)
     {
-      compare = ["1"];
+      compare = ["письмо", "письмо и свечку", "свечку"];
     }
     else if(questNum == 5)
     {
-      compare = ["1"];
+      compare = ["бутылка", "письмо", "письмо в бутылке", "бутылка вина", "бутылка из-под вина", "бутылка из-под вина с письмом", "послание"];
     }
     else if(questNum == 6)
     {
-      compare = ["1"];
+      compare = ["фонарь", "фонарик","свет", "огонь", "проводник"];
     }
     else if(questNum == 7)
     {
-      compare = ["1"];
+      compare = ["скворечник", "в скворечнике", "кормушка", " в кормушке"];
     }
     else if(questNum == 8)
     {
-     compare = ["1"];
+     compare = ["игорь", "агап", "игорь агапов"];
     }
     else if(questNum == 9)
     {
-     compare = ["1"];
+     compare = ["шкатулка", "в шкатулке", "в лесу", "в лесу в шкатулке", "шкатулка в лесу", "в шкатулке в лесу", "в лесу шкатулка", "сундук", "в сундуке", "сундучек", "сундучок", "в сундучке"];
     }
     // -- Конец ответов --
 
@@ -113,47 +113,47 @@ but.onclick = function()
     // -- Здесь все вопросы
     if(questNum == 1)
     {
-      document.getElementById("quest").innerHTML = "Вопрос 1"
+      document.getElementById("quest").innerHTML = "проверь почту. там огонь. что это?"
       document.getElementById("pic").src = "https://giffiles.alphacoders.com/811/81186.gif";
     }
     else if(questNum == 2)
     {
-      document.getElementById("quest").innerHTML = "Вопрос 2";
+      document.getElementById("quest").innerHTML = "Код - 2 е е. Зеркально, но не так просто. Введи код";
       document.getElementById("pic").src = "https://i.giphy.com/media/k3pyeP3G3UWI/giphy.webp";
     }
     else if(questNum == 3)
       {
-        document.getElementById("quest").innerHTML = "Вопрос 3";
+        document.getElementById("quest").innerHTML = "Поднимись на пятый. Там есть что-то интересное?";
         document.getElementById("pic").src = "https://i.gifer.com/Wex1.gif";
       }
       else if(questNum == 4)
         {
-          document.getElementById("quest").innerHTML = "Вопрос 4";
+          document.getElementById("quest").innerHTML = "Иди дальше наверх. Нашел?";
           document.getElementById("pic").src = "http://s1.favim.com/orig/150917/black-dark-darkness-fog-Favim.com-3310940.gif";
         }
         else if(questNum == 5)
           {
-            document.getElementById("quest").innerHTML = "Vopros 5?";
+            document.getElementById("quest").innerHTML = "Что-то у озера. Что это?";
             document.getElementById("pic").src = "https://media.giphy.com/media/1Pq6EZxEQ7sbu/giphy.gif";
           }
           else if(questNum == 6)
             {
-              document.getElementById("quest").innerHTML = "Вопрос 6";
+              document.getElementById("quest").innerHTML = "Какой подарок лежит под елкой?";
               document.getElementById("pic").src = "https://i.gifer.com/6Kn2.gif";
             }
             else if(questNum == 7)
               {
-                document.getElementById("quest").innerHTML = "Вопрос 7";
+                document.getElementById("quest").innerHTML = "Где живут птицы?";
                 document.getElementById("pic").src = "https://i.giphy.com/media/rAbKGNGM99DBC/giphy.webp";
               }
               else if(questNum == 8)
                 {
-                  document.getElementById("quest").innerHTML = "Вопрос 8";
+                  document.getElementById("quest").innerHTML = "Кто жопа";
                   document.getElementById("pic").src = "https://99px.ru/sstorage/86/2016/04/image_862504162207448079252.gif";
                 }
     else if(questNum == 9)
                 {
-                  document.getElementById("quest").innerHTML = "Вопрос 9";
+                  document.getElementById("quest").innerHTML = "Где спрятан секрет?";
                   document.getElementById("pic").src = "https://gifimage.net/wp-content/uploads/2018/11/dark-creepy-gif-4.gif";
                 }
                 // Конец вопросов
